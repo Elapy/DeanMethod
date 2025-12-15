308c82163f70c336d73f8091068bb5df826954b9
Elapy/Dean <65427843+Elapy@users.noreply.github.com>
2025-12-15 09:56:49 -0800

    Add site configuration for DeanMethod

---
+++ src/site-config.js
@@ -0,0 +1,9 @@
+window.DEANMETHOD_CONFIG = {
+  orgName: "Method",
+  brandName: "DeanMethod",
+
+  // Set these:
+  businessEmail: "method.dean@outlook.com",
+  businessPhoneDisplay: "(805) 665-3528",
+  businessPhoneE164: "+18056653528"
+};
